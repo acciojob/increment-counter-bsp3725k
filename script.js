d7ab18020589897196619a4d06fd2a71d69ba607
bsp3725k <139534037+bsp3725k@users.noreply.github.com>
2024-12-16 14:20:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 //your JS code here. If required.
 let counter = 0;
-
 // Get the button and counter elements
 const incrementBtn = document.getElementById("incrementBtn");
-const counterParagraph = document.getElementById("counter");
-const baseUrl = "http://localhost:3000 says"; 
+const counterParagraph = document.getElementById("counter"); 
 // Add event listener to the button
 incrementBtn.addEventListener("click", () => {
     // Display alert with the current counter value
-    alert(`${baseUrl} \n ${counter}`);
-
+    alert(`${counter}`);
     // Increment the counter
     counter++;
-
     // Update the counter paragraph text
     counterParagraph.textContent = counter;
 });
